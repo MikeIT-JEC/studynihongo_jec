9f33ff6d3ac2fdf7367982b2a3d3e89bee634f0b
MikeIT-JEC <mike.jec.it@gmail.com>
2016-11-09 11:55:29 +0800

    update gallery

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,5 +1,7 @@
 class Gallery < ActiveRecord::Base
 	
+	attr_accessor :image
+	
 	has_attached_file :image, styles: {large: "600x600>", medium: "300x300>", thumb: "150x150#", average: "100x100#", below_average: "70x70#", small: "30x30#"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
